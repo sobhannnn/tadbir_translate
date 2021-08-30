c6498e4e7301281158206a76440ce07dc4eb4933
Mohammad <darbanhandrew@gmail.com>
2021-08-30 09:30:39 +0430

    changed add users

--- basic/management/commands/addusers.py
+++ basic/management/commands/addusers.py
@@ -12,7 +12,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
-        data1 = json.load(codecs.open(BASE_DIR/'media/data.json', 'r', 'utf-8-sig'))
+        data1 = json.load(codecs.open(BASE_DIR / 'media/data.json', 'r', 'utf-8-sig'))
         data2 = json.dumps(data1)
         print(data1["NewDataSet"][0])
         for json_user in data1["NewDataSet"]:
@@ -21,6 +21,10 @@ class Command(BaseCommand):
             user.first_name = json_user["fisrt_name"]
             user.last_name = json_user["last_name"]
             user.age = int(json_user["age"])
+            if json_user["is_asli"] == "True":
+                user.is_asli = True
+            else:
+                user.is_asli = False
             user.roles.add(Role.objects.get(pk=1))
             user.save()
             BimeShavanadeGharardad.objects.create(bimeshavande=user, gharardad=Gharardad.objects.last())
